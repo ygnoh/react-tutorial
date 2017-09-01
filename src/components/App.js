68164b0e57376323a795e5d9092a48de98e40b13
yonggoonoh <nohyg90@gmail.com>
2017-09-01 17:36:58 +0900

    메소드 호출

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 class App extends React.Component {
+	alertMethod() {
+		let alertText = "alert fired";
+		
+		alert(alertText);
+	};
+
 	render() {
 		return (
-			<h1>Hello React Skeleton</h1>
+			<div>
+				<h1>Hello React Skeleton</h1>
+				<button onClick={this.alertMethod}>alert</button>
+			</div>
 		);
-	}
+	};
 }
 
 export default App;
